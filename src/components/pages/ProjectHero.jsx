5e43fa4ab7873a48f64a278f5c5a922bfd187963
Addison <addisonwest98@gmail.com>
2023-04-18 16:44:20 -0400

    project hero img placement

--- src/components/pages/ProjectHero.jsx
+++ src/components/pages/ProjectHero.jsx
@@ -12,7 +12,7 @@ class ProjectHero extends React.Component {
                 <div className={'py-48-154 '+(darkMode ? 'bg-black' : 'bg-white')}>
                     <Container>
                         <Row>
-                            <Col sm={4} className='copy'>
+                            <Col sm={7} className='copy'>
                                 <div className='flex text-red inter-paragraph'>
                                     <div className='mr-24'>{page.tags[0]}</div>
                                     <div className='mr-24'>{page.tags[1]}</div>
@@ -21,23 +21,31 @@ class ProjectHero extends React.Component {
                                     }
                                 </div>
                                 <div name='text-theme-toggle' className={darkMode ? 'text-white' : 'text-black'}>
-                                    <span className='h1'>{page['title-1']}</span>
-                                    <span className='flex flex-justify-end'>
-                                        <div className='h2'>{page['title-2']}</div>
-                                    </span>
-                                    <div className='inter-large my-64-32'>
-                                        {page['description']}
-                                    </div>
-                                    <div className='flex mb-54'>
-                                        <div className='mr-64'>
-                                            <div className='inter-eyebrow'>Role</div>
-                                            <div className='h4'>{page['role']}</div>
-                                        </div>
-                                        <div>
-                                            <div className='inter-eyebrow'>Timeframe</div>
-                                            <div className='h4'>{page['timeframe']}</div>
-                                        </div>
-                                    </div>
+                                    <Row>
+                                        <span className='h1'>{page['title-1']}</span>
+                                    </Row>
+                                    <Row>
+                                        <Col sm={7}>
+                                            <span className='flex flex-justify-center'>
+                                                <div className='h2'>{page['title-2']}</div>
+                                            </span>
+                                            <div className='inter-large my-64-32'>
+                                                {page['description']}
+                                            </div>
+                                            <div className='flex mb-54'>
+                                                <div className='mr-64'>
+                                                    <div className='inter-eyebrow'>Role</div>
+                                                    <div className='h4'>{page['role']}</div>
+                                                </div>
+                                                <div>
+                                                    <div className='inter-eyebrow'>Timeframe</div>
+                                                    <div className='h4'>{page['timeframe']}</div>
+                                                </div>
+                                            </div>
+                                        </Col>
+                                        
+                                    </Row>
+                                    
                                     <ArrowLogo />
                                 </div>
                             </Col>
