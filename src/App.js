a9039b7fa2a66eca6973401aed99416d496b5194
Addison <addisonwest98@gmail.com>
2024-02-08 09:46:45 -0500

    last bit of darkMode refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Navigation from './components/app/Navigation'
 import Home from './pages/Home'
 import Footer from './components/app/Footer'
@@ -10,41 +10,67 @@ import CNET from './pages/CNET'
 import { Route, Routes } from 'react-router-dom'
 import ScrollToTop from './components/app/ScrollToTop'
 
-class App extends React.Component {
+export const ThemeContext = createContext(null)
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            darkMode: true
-        }
-    }
+function App() {
 
-    toggleDarkMode() {
-        if (this.state.darkMode === true) {
-            this.setState({darkMode: false})
-        } else {
-            this.setState({darkMode: true})
-        }
-    }
+  const [darkMode, setDarkMode] = useState(true)
 
-    render() {
-      return (
-        <div className={"App "+ (this.state.darkMode ? 'bg-black' : 'bg-white')}>
-          <Navigation darkMode={this.state.darkMode} onClick={() => this.toggleDarkMode()}/>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Home darkMode={this.state.darkMode}/>} />
-            <Route path="mms" element={<MMS darkMode={this.state.darkMode}/>} />
-            <Route path="coverage" element={<Coverage darkMode={this.state.darkMode}/>} />
-            <Route path="bankrate" element={<Bankrate darkMode={this.state.darkMode}/>} />
-            <Route path="wsj" element={<WSJ darkMode={this.state.darkMode}/>} />
-            <Route path="cnet" element={<CNET darkMode={this.state.darkMode}/>} />
-          </Routes>
-          <Footer darkMode={this.state.darkMode}/>
-        </div>
-      )
-    }
-
+  return (
+    <ThemeContext.Provider value={darkMode}>
+      <div className={"App "+ (darkMode ? 'bg-black' : 'bg-white')}>
+        <Navigation onClick={() => setDarkMode(!darkMode)}/>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home darkMode={darkMode}/>} />
+          {/* <Route path="mms" element={<MMS darkMode={this.state.darkMode}/>} />
+          <Route path="coverage" element={<Coverage darkMode={this.state.darkMode}/>} />
+          <Route path="bankrate" element={<Bankrate darkMode={this.state.darkMode}/>} />
+          <Route path="wsj" element={<WSJ darkMode={this.state.darkMode}/>} />
+          <Route path="cnet" element={<CNET darkMode={this.state.darkMode}/>} /> */}
+        </Routes>
+        <Footer darkMode={darkMode}/>
+      </div>
+    </ThemeContext.Provider>
+    
+  )
 }
+// class App extends React.Component {
+
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             darkMode: true,
+//             home: true
+//         }
+//     }
+
+//     toggleDarkMode() {
+//         if (this.state.darkMode === true) {
+//             this.setState({darkMode: false})
+//         } else {
+//             this.setState({darkMode: true})
+//         }
+//     }
+
+//     render() {
+//       return (
+//         <div className={"App "+ (this.state.darkMode ? 'bg-black' : 'bg-white')}>
+//           <Navigation darkMode={this.state.darkMode} onClick={() => this.toggleDarkMode()}/>
+//           <ScrollToTop />
+//           <Routes>
+//             <Route path="/" element={<Home darkMode={this.state.darkMode}/>} />
+//             <Route path="mms" element={<MMS darkMode={this.state.darkMode}/>} />
+//             <Route path="coverage" element={<Coverage darkMode={this.state.darkMode}/>} />
+//             <Route path="bankrate" element={<Bankrate darkMode={this.state.darkMode}/>} />
+//             <Route path="wsj" element={<WSJ darkMode={this.state.darkMode}/>} />
+//             <Route path="cnet" element={<CNET darkMode={this.state.darkMode}/>} />
+//           </Routes>
+//           <Footer darkMode={this.state.darkMode}/>
+//         </div>
+//       )
+//     }
+
+// }
 
 export default App;
--- src/components/app/Navigation.jsx
+++ src/components/app/Navigation.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useContext } from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap'
+import { ThemeContext } from '../../App'
 
-class Navigation extends React.Component {
-    
-    render() {
-        const darkMode = this.props.darkMode
-        return (
+
+function Navigation() {
+
+    const darkMode = useContext(ThemeContext)
+    return (
+        <ThemeContext.Provider value={darkMode}>
             <div className='navigation'>
                 <Navbar id='navbar' className={'bar ' + (darkMode ? 'bg-black' : 'bg-white')}>
                     <Container>
@@ -18,14 +21,43 @@ class Navigation extends React.Component {
                         <Nav>
                             <Nav.Item className='flex'>
                                 <div className={'h5 ' + (darkMode ? 'text-white' : 'text-black')}>Dark mode</div>
-                                <div className={darkMode ? 'dark-mode' : 'dark-mode-off'} onClick={() => this.props.onClick()}></div>
+                                <div className={darkMode ? 'dark-mode' : 'dark-mode-off'} onClick={() => this.props.onClick}></div>
                             </Nav.Item>
                         </Nav>
                     </Container>
                 </Navbar>
             </div>
-        )
-    }
+        </ThemeContext.Provider>
+        
+    )
 }
+
+
+// class Navigation extends React.Component {
+    
+//     render() {
+//         const darkMode = this.props.darkMode
+//         return (
+//             <div className='navigation'>
+//                 <Navbar id='navbar' className={'bar ' + (darkMode ? 'bg-black' : 'bg-white')}>
+//                     <Container>
+//                         <Nav className="me-auto">
+//                             <Nav.Link href="" className='h5 text-red brand'>Abby Moulton</Nav.Link>
+//                             <Nav.Link href="" className={'h5 navigation-link ' + (darkMode ? 'text-white' : 'text-black')}>Work</Nav.Link>
+//                             <Nav.Link href="" className={'h5 navigation-link ' + (darkMode ? 'text-white' : 'text-black')}>About</Nav.Link>
+//                             <Nav.Link href="" className={'h5 navigation-link ' + (darkMode ? 'text-white' : 'text-black')}>Resume</Nav.Link>
+//                         </Nav>
+//                         <Nav>
+//                             <Nav.Item className='flex'>
+//                                 <div className={'h5 ' + (darkMode ? 'text-white' : 'text-black')}>Dark mode</div>
+//                                 <div className={darkMode ? 'dark-mode' : 'dark-mode-off'} onClick={() => this.props.onClick()}></div>
+//                             </Nav.Item>
+//                         </Nav>
+//                     </Container>
+//                 </Navbar>
+//             </div>
+//         )
+//     }
+// }
 
 export default Navigation
